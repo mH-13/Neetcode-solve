3a5ce325695ab2a40b82e2596c554ba163284bfd
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-03-14 23:57:39 +0600

    dummy

--- dummy.py
+++ dummy.py
@@ -23,27 +23,7 @@ for i in range(n-1,-1, -1):
     if (i == n-1):
         postfix[i] = nums[i]
     else:
-        postf = nums[i] * postfix(i+1)
+        postf = nums[i] * postfix[i+1]
         postfix[i] = postf
         
 print(postfix)
-
-# for i in range (0, len(nums)):
-#     if i == 0:
-#         res.append(nums[i])
-#     elif i == len(nums):
-#         res.append(nums(i))
-#     else:
-#         prdct = prefix(i-1) * postfix(i+1)
-#         res.append(prdct)
-
-
-# for i in range(0, len(nums)):
-#     print(i, nums[i])
-
-# print(" -------------- ")
-
-
-    
-# for i in range(len(nums)-1, -1, -1):
-#     print(i, nums[i])
