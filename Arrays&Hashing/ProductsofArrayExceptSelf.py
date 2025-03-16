204994202d807cb56cd55f41cd016d424c0ad728
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-03-16 20:34:29 +0600

    prefixPostfix

--- Arrays&Hashing/ProductsofArrayExceptSelf.py
+++ Arrays&Hashing/ProductsofArrayExceptSelf.py
@@ -69,12 +69,12 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
         n = len(nums)
-        res = [1] * n
+        res = [1] * n #
         
         prefix = 1
         for i in range(0, n):
             res[i] = prefix
-            prefix  *= nums[i]
+            prefix  *= nums[i] #here, 
 
         postfix = 1
         for i in range(n-1, -1, -1):
@@ -82,3 +82,6 @@ class Solution:
             postfix *= nums[i]
 
         return res
+    
+    
+    
