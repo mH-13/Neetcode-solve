2635bc0b12297ee5d07fc86f94b989400b1acd35
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-03-18 00:01:21 +0600

    updated

--- 10.validPalindrome.py
+++
@@ -1,3 +0,0 @@
-#valid Palindrome
-
-#Leetcode problem link: https://leetcode.com/problems/valid-palindrome/
---
+++ TwoPointer/10.validPalindrome.py
@@ -0,0 +1,26 @@
+#valid Palindrome
+
+#Leetcode problem link: https://leetcode.com/problems/valid-palindrome/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Input: s = "Was it a car or a cat I saw?"
+
+# Output: true
+
+# Input: s = "tab a cat"
+
+# Output: false
