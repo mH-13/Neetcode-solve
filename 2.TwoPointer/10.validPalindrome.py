213ccd549b8a201d84e3e51a97486214144a6c97
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-03-19 05:49:51 +0600

    udpated

--- 2.TwoPointer/10.validPalindrome.py
+++ 2.TwoPointer/10.validPalindrome.py
@@ -3,24 +3,19 @@
 #Leetcode problem link: https://leetcode.com/problems/valid-palindrome/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Input: s = "Was it a car or a cat I saw?"
-
-# Output: true
-
-# Input: s = "tab a cat"
-
-# Output: false
+from typing import List
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstr = ""
+        
+        for c in s:
+            if c.isalnum():
+                newstr += c.lower()
+                
+            return newstr == newstr[::-1]
+        
+if __name__ == "__main__":
+    solution = Solution()
+    input_str = "Was it a car or a cat I saw?"
+    result = solution.isPalindrome(input_str)
+    print(result)
