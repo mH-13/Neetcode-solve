66233717b096bbda5753e56e477a5bce72d1cc41
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-03-22 13:53:54 +0600

    3sum

--- 2.TwoPointer/12.3sum.py
+++ 2.TwoPointer/12.3sum.py
@@ -40,3 +40,29 @@ class Solution:
 #Time complexity: O(n^2) and space complexity: O(1) so this is optimal solution
 #this approach is better than the previous one because we are using two pointer approach to solve the problem and we are also sorting the array to avoid duplicates in the result 
 
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(0, len(nums)-2):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+
+            l = i+1
+            r = len(nums) - 1 
+
+            while l<r:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    
+                    while l<r and nums[l] == nums[l+1]:
+                        l += 1
+                    l+=1
+                elif(nums[i] + nums[l] + nums[r] > 0 ):
+                    r -= 1
+                elif(nums[i] + nums[l] + nums[r] < 0):
+                    l += 1        
+        return res
+
+        
