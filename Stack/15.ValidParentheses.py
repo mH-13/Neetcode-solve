4e55a42c46034c8f125f2311a542be1f9c55029e
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-03-26 17:04:52 +0600

    updated

---
+++ Stack/15.ValidParentheses.py
@@ -0,0 +1,32 @@
+#leetcode link: https://leetcode.com/problems/valid-parentheses/
+
+
+from  typing import List
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        p = {")" : "(", "}" : "{", "]" : "["}
+        stack = []
+        for i in range(n):
+            if s[i] in p:
+                # print('s[i] ', s[i])
+                # print('p[s[i]] ', p[s[i]])
+                # print('stack ', stack)
+                # print('stack[-1] ', stack[-1])
+                if stack and stack[-1] == p[s[i]]: #if stack is not empty and stack[-1] is equal to p[s[i]]
+                    stack.pop()
+                else:
+                    return False
+            else:
+                # print('else : s[i] ', s[i])
+                stack.append(s[i])
+        
+        return not stack
+
+if __name__ == "__main__":
+    solution = Solution()
+    s="([{}])"
+    print(solution.isValid(s))
+    
--- dummy.py
+++ dummy.py
@@ -1,41 +1,31 @@
 from typing import List
 
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        total = 0
-        left_max = [0] * len(height)
-        right_max = [0] * len(height)
-        minlr = [0] * len(height)
-
-        lftmx = 0
-        rhsmx = 0
-        for i in range(0, len(height)-1):
-            lftmx = max(lftmx, height[i])
-            left_max[i] = lftmx
-
-        for i in range(len(height)-1,-1,-1): # to fix this line of code we need to change the range to len(height)-1, -1, -1  and also change the range of the for loop to 0, len(height)-1
-
-            rhsmx = max(rhsmx, height[i])
-            right_max[i] = rhsmx
-        
-        for i in range(0, len(height)):
-            minlr[i] = min(left_max[i], right_max[i])
-            
-        print('left_max:', left_max)
-        print('right_max:', right_max)
-        print('minlr:', minlr)
-
-            
-
-        
-
-        
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        p = {")" : "(", "}" : "{", "]" : "["}
+        l = 0
+        r = n-1
+        c = 0
+        while l<r:
+            if(s[l] != p[s[r]]):
+                return False
+            else:
+                # print('s[l] ', s[l], ' p[s[r]] ', p[s[r]], 'c ', c)
+                c += 1
+                l += 1
+                r -= 1
+        if c == n /2:
+            return True
+        else:
+            # print('c ', c, 'n ', n)
+            return False
 
 
 if __name__ == "__main__":
     solution = Solution()
-    height = [0,2,0,3,1,0,1,3,2,1]
-    solution.trap(height)
+    s="()[]{}"
+    print(solution.isValid(s))
     
     
     
