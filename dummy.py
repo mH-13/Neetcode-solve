b527a4aa2d1279eebcb7e0a839781fa9734d34fc
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-04-01 02:42:09 +0600

    updated

--- dummy.py
+++ dummy.py
@@ -1,31 +1,29 @@
 from typing import List
 
 class Solution:
-    def isValid(self, s: str) -> bool:
-        n = len(s)
-        p = {")" : "(", "}" : "{", "]" : "["}
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
         l = 0
-        r = n-1
-        c = 0
-        while l<r:
-            if(s[l] != p[s[r]]):
-                return False
+        r = 1
+        profit = 0
+        while r<=n-1:
+            print('l ', l, ' r ', r, 'prices[l] ', prices[l], ' prices[r] ', prices[r])
+            pr = max(0, prices[r] - prices[l])
+            profit = max(profit, pr)
+            
+            if prices[l] > prices[r]:
+                l+=1
+                r += 1                
             else:
-                # print('s[l] ', s[l], ' p[s[r]] ', p[s[r]], 'c ', c)
-                c += 1
-                l += 1
-                r -= 1
-        if c == n /2:
-            return True
-        else:
-            # print('c ', c, 'n ', n)
-            return False
-
+                r += 1
+        return profit
 
 if __name__ == "__main__":
     solution = Solution()
-    s="()[]{}"
-    print(solution.isValid(s))
+    # prices=[5,1,5,6,7,1,10]
+    prices=[2,1,2,1,0,1,2]
+    # prices=[7,1,5,3,6,4]
+    print(solution.maxProfit(prices))
     
     
     
