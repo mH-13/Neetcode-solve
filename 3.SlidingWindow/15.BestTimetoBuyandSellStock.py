e62072e607fd24bea62bfccc24f66185aba3f3ac
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-04-01 11:35:26 +0600

    updated

--- 3.SlidingWindow/15.BestTimetoBuyandSellStock.py
+++ 3.SlidingWindow/15.BestTimetoBuyandSellStock.py
@@ -25,8 +25,35 @@ class Solution:
 
 
 #optimized solution using two pointer approach:
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 1:
+            return 0
+        if n == 2:
+            return max(0, prices[1] - prices[0])
+        l = prices[0]
+        r = prices[1]
+        profit = 0
+        for i in range(1, n-1):
+            # print('outer i - ', i)
+            pr = max(0, r - l)
+            profit = max(profit, pr)
+            if l >= r:
+                # print('l ', l, ' r ', r)
+                # print('i - ', i)
+                l = r
+                r = prices[i+1]              
+            else:
+                r = prices[i+1]
+            pr = max(0, r - l)
+            profit = max(profit, pr)
+            
+            # print('lastt l ', l, ' r ', r)
+            # print('----------------------------------------------')
+        return profit
 
-
+#Time complexity: O(n), Space complexity: O(1) but still not optimal
 
 
 if __name__ == "__main__":
--- dummy.py
+++ dummy.py
@@ -3,25 +3,40 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        l = 0
-        r = 1
+        if n == 1:
+            return 0
+        if n == 2:
+            return max(0, prices[1] - prices[0])
+        
+        l = prices[0]
+        r = prices[1]
         profit = 0
-        while r<=n-1:
-            print('l ', l, ' r ', r, 'prices[l] ', prices[l], ' prices[r] ', prices[r])
-            pr = max(0, prices[r] - prices[l])
+        for i in range(1, n-1):
+            # print('outer i - ', i)
+            print('l ', l, ' r ', r)
+            
+            pr = max(0, r - l)
             profit = max(profit, pr)
             
-            if prices[l] > prices[r]:
-                l+=1
-                r += 1                
+            if l >= r:
+                # print('l ', l, ' r ', r)
+                # print('i - ', i)
+                l = r
+                r = prices[i+1]              
             else:
-                r += 1
+                r = prices[i+1]
+                
+            pr = max(0, r - l)
+            profit = max(profit, pr)
+            
+            # print('lastt l ', l, ' r ', r)
+            # print('----------------------------------------------')
         return profit
 
 if __name__ == "__main__":
     solution = Solution()
-    # prices=[5,1,5,6,7,1,10]
-    prices=[2,1,2,1,0,1,2]
+    prices=[1,2]
+    # prices=[2,1,2,1,0,1,2]
     # prices=[7,1,5,3,6,4]
     print(solution.maxProfit(prices))
     
