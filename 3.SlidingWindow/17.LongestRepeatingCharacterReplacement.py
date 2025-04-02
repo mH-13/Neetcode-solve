d86ac4efd77a058915d3a34b4e6a7d6b5aedaf00
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-04-02 23:08:14 +0600

    updated

---
+++ 3.SlidingWindow/17.LongestRepeatingCharacterReplacement.py
@@ -0,0 +1,12 @@
+#leetcode link: https://leetcode.com/problems/longest-repeating-character-replacement/
+# This is a variation of the above problem. The only difference is that we are allowed to replace k characters in the window to make all characters same.
+# The solution is almost same as above. The only difference is that we need to keep track of the character with maximum frequency in the window.
+# If the window size - max frequency > k, then we need to shrink the window.
+# The final answer is the maximum window size we have seen so far.
+
+from typing import List
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+      
