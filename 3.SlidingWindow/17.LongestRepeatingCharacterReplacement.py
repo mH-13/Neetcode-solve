df683ba4536ef8fbc4154cfee7dc3592ac9c6567
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-04-04 13:50:41 +0600

    updated

--- 3.SlidingWindow/17.LongestRepeatingCharacterReplacement.py
+++ 3.SlidingWindow/17.LongestRepeatingCharacterReplacement.py
@@ -10,3 +10,20 @@ from typing import List
 
 # brute force solution O(n^2)
 
+def characterReplacement(s: str, k: int) -> int:
+  max_len = 0
+  max_freq = 0
+  count = {}
+  left = 0
+
+  for right in range(len(s)):
+    count[s[right]] = count.get(s[right], 0) + 1
+    max_freq = max(max_freq, count[s[right]])
+
+    while (right - left + 1) - max_freq > k:
+      count[s[left]] -= 1
+      left += 1
+
+    max_len = max(max_len, right - left + 1)
+
+  return max_len
